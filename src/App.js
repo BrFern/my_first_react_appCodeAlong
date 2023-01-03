c377b34f9bb4089d265b6168ca8def92860f7128
Bailey <baileyrosefern@gmail.com>
2023-01-03 14:30:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Remember this is not HTML, this is JS 
+// Can create the variables and then make ternary operators to 
+//You can't do this easily with the other one. 
+
+//Originally in the other part, just showing some usage:
+// const isNameShowing = true;
+//{isNameShowing ? name: 'someone'}
+
+//<></> React fragment-essentially a div
+
+//Adjacent JSX elements must be wrapped in an enclosing tag, 
+//Cannot have two heading in one place? Need to use a react fragment
+
+
+const App = () => {
+  const name = 'Jane';
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1> Hello, {2+2}!</h1>
+      {name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+      ): (
+        <>
+        <h1>test</h1>
+        <h2>There is no name</h2>
+        </>
+      )}
     </div>
   );
 }
